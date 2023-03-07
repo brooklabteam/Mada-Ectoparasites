3c1c6998fd29472ca024166a81a0499f3104c773
Kiy761 <46729755+Kiy761@users.noreply.github.com>
2023-03-07 11:21:22 -0700

    Updated temp point code and data

--- Climate-data/Code/Temp_point.R
+++ Climate-data/Code/Temp_point.R
@@ -55,48 +55,5 @@ for (i in files) {
 ls(Temp_dat)
 
 #Write out file with all NDVI data for sampling point location to table
-write.csv(Temp_dat, "C:/Users/kathe/Documents/GitHub/Mada-Ectoparasites/Climate-data/Climate_tables/Temp_point_new.csv")
+write.csv(Temp_dat, "C:/Users/kathe/Documents/GitHub/Mada-Ectoparasites/Climate-data/Climate_tables/Temp_point.csv")
 
-
-# 
-# 
-# 
-# #Rename the layers to shortent the file name
-# 
-# #original names
-# names(Temp_stk)
-# 
-# #rename
-# names(Temp_stk) <- paste0("Temp_pt_",substr((files),63,68))
-# 
-# #new names
-# names(Temp_stk)
-# 
-# # Open bat sampling site shapefile (points to extract data)
-# Sites <- readOGR(dsn = "Shapefiles/Bat_sampling_locations.shp", layer = "Bat_sampling_locations")
-# plot(Sites)
-# crs(Sites)
-# 
-# Sites_table <- as.data.frame(Sites)
-# 
-# #Ensure that raster and point file have matching CRS. TRUE = matching coordinate reference system (crs)
-# compareCRS(Temp_stk,Sites)
-# 
-# #Sanity check
-# plot(Temp_stk,1)
-# points(Sites)
-# 
-# #######################################################################
-# 
-# # Extract raster data using points
-# Temp_pt <- extract(Temp_stk, Sites)
-# Temp_pt <- as.data.frame(Temp_pt)
-# 
-# #Extract Site_ID and joine to NDVI_data
-# site_list <-as.data.frame(Sites)[1]
-# 
-# Temp_pt_extr <- cbind(site_list,Temp_pt)
-# 
-# #Write out file with all NDVI data for sampling point location to table
-# write.csv(Temp_pt_extr, "C:/Users/kathe/Documents/GitHub/Mada-Ectoparasites/Climate-data/Climate_tables/Temp_point.csv")
-# 
